a19f0f87de6f344e518aae2104ad93817b55ef33
Callum Fortune <callumfortune03@outlook.com>
2024-07-02 19:41:23 +0100

    looking good

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { DocumentDuplicateIcon, OfficeBuildingIcon } from '@heroicons/react/outline'; // Ensure you have Heroicons installed
+import { toast, Toaster } from 'react-hot-toast';
 
 const COUNTDOWN_TIME = 120; // Countdown time in seconds (e.g., 120 seconds for 2 minutes)
 
@@ -43,10 +44,12 @@ export default function Home() {
       setLoadingInbox(false);
       setLoadingEmail(true);
       pollForVerificationData(data.inboxId);
+      toast.success('Email generated successfully!');
     } catch (error: any) {
       setVerificationData(`Error: ${error.message}`);
       setLoadingInbox(false);
       setLoadingEmail(false);
+      toast.error(`Error generating email: ${error.message}`);
     }
   };
 
@@ -55,14 +58,19 @@ export default function Home() {
       const response = await fetch(`/api/get-verification-data?inboxId=${inboxId}`);
       if (response.ok) {
         const data = await response.json();
-        console.log(JSON.stringify(data));
         let displayContent;
+        const companyInfo = data.company ? (
+          <span className='px-4 py-2 rounded-lg bg-gray-100 text-center font-bold self-end'>{data.company}</span>
+        ) : (
+          <span className='px-4 py-2 rounded-lg bg-red-100 text-center font-bold self-end'>Company information unavailable</span>
+        );
+
         if (data.link) {
           displayContent = (
             <div className='flex mt-8 gap-4 items-end'>
               <div className='flex items-center gap-2'>
                 <OfficeBuildingIcon className="w-5 h-5" />
-                <span className='px-4 py-2 rounded-lg bg-gray-100 text-center font-bold self-end'>{data.company || 'No company data available'}</span>
+                {companyInfo}
               </div>
               <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg self-end" onClick={() => window.open(data.link, "_blank")}>
                 Verify Link
@@ -74,15 +82,12 @@ export default function Home() {
             <div className='flex mt-8 gap-4 items-end'>
               <div className='flex items-center gap-2'>
                 <OfficeBuildingIcon className="w-5 h-5" />
-                <span className='px-4 py-2 rounded-lg bg-gray-100 text-center font-bold self-end'>{data.company || 'No company data available'}</span>
+                {companyInfo}
               </div>
               <div onClick={() => navigator.clipboard.writeText(data.code)} className="relative px-4 py-2 border rounded-lg bg-gray-100 hover:bg-gray-300 hover:scale-[1.05] duration-75 cursor-pointer self-end">
                 {data.code}
                 <div className='p-1 rounded-lg bg-gray-100 text-gray-600 absolute top-0 right-0 translate-y-[-50%] translate-x-[50%]'>
-                  <DocumentDuplicateIcon
-                    className="w-5 h-5 cursor-pointer"
-                    onClick={() => navigator.clipboard.writeText(data.code)}
-                  />
+                  <DocumentDuplicateIcon className="w-5 h-5 cursor-pointer" onClick={() => navigator.clipboard.writeText(data.code)} />
                 </div>
               </div>
             </div>
@@ -96,24 +101,32 @@ export default function Home() {
         }
         setVerificationData(displayContent);
         setLoadingEmail(false);
+        toast.success('Verification data received successfully!');
       } else {
         setTimeout(() => pollForVerificationData(inboxId), 2000); // Retry after 2 seconds
+        toast.error('Verification data not yet available, retrying...');
       }
     } catch (error: any) {
       setVerificationData(`Error: ${error.message}`);
       setLoadingEmail(false);
+      toast.error(`Error retrieving verification data: ${error.message}`);
     }
   };
 
   return (
     <div className="relative flex flex-col min-h-screen">
-      <div className='w-screen h-[300px] bg-white grid place-content-center'>
-        <h1 className='text-green-600 text-[4em] font-bold'>Phantom<span className='text-gray-600'>Sign</span></h1>
+      <Toaster />
+      <div className='w-screen h-[300px] bg-white flex flex-col items-start justify-start md:items-center md:justify-center px-[5%] py-2'>
+        <h1 className='text-green-600 text-[1.5em] md:text-[4em] font-bold relative'>Phantom<span className='text-gray-600'>Sign</span>
+          <span className='opacity-90 text-gray-600 hidden md:block text-[0.3em] font-medium absolute bottom-0 right-0 transform translate-y-[30%] translate-x-[50%] rotate-[-10deg] border-4 font-bold border-red-500 text-red-500 px-4 py-2 rounded-lg'>
+            F*CK SPAM
+          </span>
+        </h1>
       </div>
 
-      <div className="relative flex flex-col flex-grow items-center bg-gray-200 pt-[100px]">
+      <div className="relative flex flex-col flex-grow items-center bg-gray-200">
         <button
-          className="absolute bg-blue-500 hover:bg-blue-700 text-white text-[1.5em] font-bold py-2 px-4 rounded-lg flex items-center justify-center"
+          className="shimmery-button absolute text-white text-[1.5em] font-bold py-2 px-4 rounded-lg flex items-center justify-center"
           style={{ top: '0', transform: 'translateY(-50%)' }}
           onClick={generateEmail}
           disabled={loadingInbox}
