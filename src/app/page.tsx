04469c86f14eaaa2b2cb7469fae0dd6cb1f76e5b
Callum Fortune <callumfortune03@outlook.com>
2024-07-04 00:02:22 +0100

    add supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { useState, useEffect } from 'react';
-import { DocumentDuplicateIcon, OfficeBuildingIcon, SparklesIcon } from '@heroicons/react/outline'; // Ensure you have Heroicons installed
+import { ExternalLinkIcon, DocumentDuplicateIcon, OfficeBuildingIcon, SparklesIcon } from '@heroicons/react/outline';
 import { toast, Toaster } from 'react-hot-toast';
 import Image from 'next/image';
 import logo from '../../public/phantom.svg';
@@ -9,11 +10,28 @@ import logo from '../../public/phantom.svg';
 const COUNTDOWN_TIME = 300; // Countdown time in seconds (e.g., 300 seconds for 5 minutes)
 
 export default function Home() {
+  const supabase = createClientComponentClient();
   const [email, setEmail] = useState('');
   const [verificationData, setVerificationData] = useState<string | JSX.Element>('');
   const [loadingInbox, setLoadingInbox] = useState(false);
   const [loadingEmail, setLoadingEmail] = useState(false);
   const [countdown, setCountdown] = useState(COUNTDOWN_TIME);
+  const [emailCount, setEmailCount] = useState<number>(0);
+
+  useEffect(() => {
+    const fetchEmailCount = async () => {
+      const { count, error } = await supabase
+        .from('generatedEmails')
+        .select('*', { count: 'exact', head: true });
+      if (error) {
+        console.error('Error fetching email count:', error);
+      } else {
+        count && setEmailCount(count);
+      }
+    };
+
+    fetchEmailCount();
+  }, []);
 
   useEffect(() => {
     let timer: NodeJS.Timeout;
@@ -43,6 +61,22 @@ export default function Home() {
       });
       const data = await response.json();
       setEmail(data.emailAddress);
+
+      // Save the generated email to Supabase
+      const { error } = await supabase
+        .from('generatedEmails')
+        .insert([{ email: data.emailAddress }]);
+      if (error) {
+        console.error('Error saving email to Supabase:', error);
+        toast.error(`Error saving email: ${error.message}`);
+      } else {
+        // Fetch updated email count
+        const { count } = await supabase
+          .from('generatedEmails')
+          .select('*', { count: 'exact', head: true });
+        count && setEmailCount(count);
+      }
+
       setLoadingInbox(false);
       setLoadingEmail(true);
       pollForVerificationData(data.inboxId);
@@ -72,11 +106,18 @@ export default function Home() {
 
         if (data.link) {
           displayContent = (
-            <div className='flex mt-8 gap-4 items-end'>
-              {companyInfo}
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg self-end" onClick={() => window.open(data.link, "_blank")}>
-                Verify Link
-              </button>
+            <div className='flex flex-col'>
+              <p className='mt-8 text-center'>Email received</p>
+              <div className='flex flex-col md:flex-row mt-8 gap-4 items-end'>
+                {companyInfo}
+                <button
+                  className="w-full md:w-[auto] bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg self-end flex items-center"
+                  onClick={() => window.open(data.link, "_blank")}
+                >
+                  Verify Link
+                  <ExternalLinkIcon className="w-5 h-5 ml-2" />
+                </button>
+              </div>
             </div>
           );
         } else if (data.code) {
@@ -124,108 +165,172 @@ export default function Home() {
         <a href="https://seefortune.co.uk" className='hidden'/>
         <Toaster />
         <div className='w-screen h-[300px] bg-white flex flex-col items-center justify-center px-[5%] py-2'>
-
-
-
           <div className='absolute top-0 left-0 w-screen flex items-center justify-between px-[5%] pt-2'>
-
             <div>
-
               <Image 
                 src={logo} 
                 alt="PhantomSign Logo" 
                 width={50} 
                 height={50}
-                className=''
+                className=']'
               />
-              
             </div>
-
-            <ul className='flex gap-8 md:gap-16 text-gray-600'>
+            <ul className='flex pr-2 md:pr-[auto] gap-8 md:gap-16 text-gray-600 font-bold'>
               <li><a href="#about">What is this</a></li>
               <li><a href="#instructions">How to use</a></li>
             </ul>
-
           </div>
-
-
-
-
           <h1 className='text-green-600 text-[2.5em] md:text-[4em] font-bold relative'>Phantom<span className='text-gray-600'>Sign</span>
-            {/* <span className='opacity-90 text-gray-600 hidden md:block text-[0.3em] font-medium absolute bottom-0 right-0 transform translate-y-[30%] translate-x-[50%] rotate-[-10deg] border-4 font-bold border-red-500 text-red-500 px-4 py-2 rounded-lg'>
-              F*CK SPAM
-            </span> */}
           </h1>
         </div>
+        <div className="relative flex flex-col items-center bg-gray-200 flex-grow">
 
-        <div className="relative flex flex-col flex-grow items-center bg-gray-200">
-          <div className='float-animation absolute'>
-            <Image 
-              src={logo} 
-              alt="PhantomSign Logo" 
-              width={100} 
-              height={100}
-              className='translate-y-[-80%]'
-            />
+          <div className='absolute'>
+            <div className='w-full flex justify-center float-animation absolute -translate-y-[50%] -top-[80px]'>
+              <Image 
+                src={logo} 
+                alt="PhantomSign Logo" 
+                width={100} 
+                height={100}
+                className=''
+              />
+            </div>
+            <button
+              className="shimmery-button simple-shadow absolute -translate-y-[50%] text-white text-[1.5em] font-bold py-2 px-6 rounded-lg flex items-center justify-center"
+              onClick={generateEmail}
+              disabled={loadingInbox}
+            >
+              {loadingInbox ? (
+                <>
+                  <svg className="animate-spin h-5 w-5 mr-3 border-4 border-t-4 border-gray-200 border-t-white rounded-full" viewBox="0 0 24 24"></svg>
+                  Generating...
+                </>
+              ) : email ? (
+                'Regenerate Email'
+              ) : (
+                <span className='flex items-center'>
+                  Generate Email
+                  <SparklesIcon className="w-5 h-5 ml-4 scale-[1.2]" />
+                </span>
+              )}
+            </button>
           </div>
-          <button
-            className="shimmery-button simple-shadow absolute text-white text-[1.5em] font-bold py-2 px-6 rounded-lg flex items-center justify-center"
-            style={{ top: '0', transform: 'translateY(-50%)' }}
-            onClick={generateEmail}
-            disabled={loadingInbox}
-          >
-            {loadingInbox ? (
-              <>
-                <svg className="animate-spin h-5 w-5 mr-3 border-4 border-t-4 border-gray-200 border-t-white rounded-full" viewBox="0 0 24 24"></svg>
-                Generating...
-              </>
-            ) : email ? (
-              'Regenerate Email'
-            ) : (
-              <span className='flex items-center'>
-                Generate Email
-                <SparklesIcon className="w-5 h-5 ml-4 scale-[1.2]" />
-              </span>
-            )}
-          </button>
 
-          {email && (
-            <div className='flex flex-col mt-8'>
-              {/* <h2 className='text-center font-bold'>Email Address</h2> */}
-              <div onClick={() => {
-                navigator.clipboard.writeText(email);
-                toast.success("Copied to clipboard");
-                }} className="relative text-[1em] md:text-[1.4em] mt-4 px-4 py-2 border rounded-lg bg-white border border-gray-400 hover:scale-[1.05] duration-75 cursor-pointer">
-                {email}
-                <div className='p-1 rounded-lg bg-white text-gray-600 absolute top-0 right-0 translate-y-[-50%] translate-x-[50%]'>
-                  <DocumentDuplicateIcon
-                    className="w-5 h-5 cursor-pointer"
-                    onClick={() => {
-                      navigator.clipboard.writeText(email);
-                      toast.success("Copied to clipboard");
-                    }}
-                  />
+          <div className='relative w-full flex-grow flex flex-col items-center justify-center'>
+
+            <div className='w-full flex flex-col items-center'>
+              {!email && !loadingInbox && (
+                <></>
+              )}
+
+              {!email && !loadingInbox && emailCount > 0 && (
+                <div className='flex flex-col md:flex-row gap-12 py-12 pb-8'>
+
+                  <div className='flex flex-col items-center bg-white rounded-lg simple-shadow p-4 w-[180px]' style={{ aspectRatio: 1 }}>
+                    <h2 className='text-[1em]'>Codes found</h2>
+                    <h2 className='text-[4em] text-gray-600'>{emailCount}</h2>
+                  </div>
+
+                  <div className='flex flex-col items-center bg-white rounded-lg simple-shadow p-4 w-[180px]' style={{ aspectRatio: 1, transform: 'scale(1.2)' }}>
+                    <h2 className='text-[1em]'>Emails generated</h2>
+                    <h2 className='text-[4em] text-gray-600'>{emailCount}</h2>
+                  </div>
+
+                  <div className='flex flex-col items-center bg-white rounded-lg simple-shadow p-4 w-[180px]' style={{ aspectRatio: 1 }}>
+                    <h2 className='text-[1em]'>Links found</h2>
+                    <h2 className='text-[2em] md:text-[4em] text-gray-600'>{emailCount}</h2>
+                  </div>
+
                 </div>
-              </div>
-            </div>
-          )}
+              )}
 
-          {loadingEmail && (
-            <div className='flex flex-col mt-8 items-center'>
-              <svg className="animate-spin h-5 w-5 mr-3 border-4 border-t-4 border-gray-200 border-t-green-600 rounded-full" viewBox="0 0 24 24"></svg>
-              <p className='mt-8'>Deleting email in</p>
-              <p className='mt-2 text-[2em] text-gray-600'>{Math.floor(countdown / 60)}:{String(countdown % 60).padStart(2, '0')}</p>
-            </div>
-          )}
+              {email && (
+                <div className='flex flex-col'>
+                  <div onClick={() => {
+                    navigator.clipboard.writeText(email);
+                    toast.success("Copied to clipboard");
+                    }} className="relative text-[1em] md:text-[1.4em] mt-4 px-4 py-2 border rounded-lg bg-white border border-gray-400 hover:scale-[1.05] duration-75 cursor-pointer">
+                    {email}
+                    <div className='p-1 rounded-lg bg-white text-gray-600 absolute top-0 right-0 translate-y-[-50%] translate-x-[50%]'>
+                      <DocumentDuplicateIcon
+                        className="w-5 h-5 cursor-pointer"
+                        onClick={() => {
+                          navigator.clipboard.writeText(email);
+                          toast.success("Copied to clipboard");
+                        }}
+                      />
+                    </div>
+                  </div>
+                </div>
+              )}
 
-          {verificationData}
+              {loadingEmail && (
+                <div className='flex flex-col items-center mt-6'>
+                  <svg className="animate-spin h-5 w-5 mr-3 border-4 border-t-4 border-gray-200 border-t-green-600 rounded-full" viewBox="0 0 24 24"></svg>
+                  <p className='mt-8'>Deleting email in</p>
+                  <p className='mt-2 text-[2em] text-gray-600'>{Math.floor(countdown / 60)}:{String(countdown % 60).padStart(2, '0')}</p>
+                </div>
+              )}
+
+              {verificationData}
+            </div>
+          </div>
         </div>
       </div>
 
-      <div id="about" className='relative flex flex-col min-h-[50vh]'></div>
+      <div className='flex flex-col md:flex-row justify-center items-start gap-8 px-[5%] pt-8'>
+        <div id="about" className='relative flex flex-col p-6 bg-white rounded-lg shadow-lg w-full md:w-[45%] text-center'>
+          <h1 className='text-[1.5em] font-bold mb-6'>What is PhantomSign?</h1>
+          <p className='text-gray-600'>
+            PhantomSign is your digital ally against spam and unwanted emails! 
+            It creates secret email addresses that self-destruct after a set 
+            time, keeping your main inbox clean. Use PhantomSign&apos;s temporary 
+            addresses for sign-ups to hide your personal email from prying eyes 
+            and pesky marketers. Plus, PhantomSign automatically extracts verification 
+            codes or links from emails, saving you time. With a sprinkle of magic and a 
+            dash of phantom stealth, PhantomSign keeps your online presence secure and 
+            your inbox sparkling. Welcome to the future of email privacy!
+          </p>
+        </div>
 
-      <div id="instructions" className='relative flex flex-col min-h-[50vh] bg-gray-200'></div>
-
+        <div id="instructions" className='relative flex flex-col p-6 bg-white rounded-lg shadow-lg w-full md:w-[45%] text-center'>
+          <h1 className='text-[1.5em] font-bold mb-6'>How does it work?</h1>
+          <div className="flex flex-col items-center gap-4">
+            <div className="flex flex-col items-center gap-4">
+              <div className="flex items-center justify-center w-10 h-10 rounded-full text-gray-600 font-bold">
+                1
+              </div>
+              <p className='text-gray-600'>Click the "Generate Email" button to create a temporary email address.</p>
+            </div>
+            <div className="flex flex-col items-center gap-4">
+              <div className="flex items-center justify-center w-10 h-10 rounded-full text-gray-600 font-bold">
+                2
+              </div>
+              <p className='text-gray-600'>Use this temporary email address for any online sign-ups or verifications.</p>
+            </div>
+            <div className="flex flex-col items-center gap-4">
+              <div className="flex items-center justify-center w-10 h-10 rounded-full text-gray-600 font-bold">
+                3
+              </div>
+              <p className='text-gray-600'>PhantomSign monitors the inbox and automatically extracts verification codes or links from incoming emails, displaying them on the screen.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer className='bg-gray-200 text-gray-600 py-8 mt-12'>
+        <div className='container mx-auto px-4'>
+          <div className='flex flex-col md:flex-row justify-between items-center'>
+            <div className='text-center md:text-left'>
+              <p>&copy; 2024 PhantomSign. All rights reserved.</p>
+            </div>
+            <div className='flex gap-4 mt-4 md:mt-0'>
+              {/* <a href="#" className='hover:text-gray-400'>Privacy Policy</a>
+              <a href="#" className='hover:text-gray-400'>Terms of Service</a>
+              <a href="#" className='hover:text-gray-400'>Contact Us</a> */}
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
