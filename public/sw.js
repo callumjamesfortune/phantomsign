dfe15bd29ecfaee0745c58d53057165c340b5df8
Callum Fortune <callumfortune03@outlook.com>
2024-07-07 04:27:28 +0100

    uvbf

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,6 @@ self.addEventListener('push', function (e) {
     console.log('Push Received...')
     const data = e.data.json()
     self.registration.showNotification(data.title, {
-      body: 'Notified by PhantomSign',
-      icon: '/phantom.svg',
+      body: 'Notified by PhantomSign'
     })
   })
