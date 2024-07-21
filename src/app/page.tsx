c98974dfc30f6f6771ed97c6070ab720143960e6
Callum Fortune <callumfortune03@outlook.com>
2024-07-21 05:10:32 +0100

    ermmm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -441,7 +441,7 @@ export default function Home() {
         <div className='w-full flex items-center justify-end'>
           <h1 className='text-gray-200 px-8 text-[4em]'>2</h1>
           <div className='w-full md:w-1/2 items-center bg-white rounded-lg simple-shadow p-4'>
-            <h2 className='text-[1.5em] mb-2'>Use it for signup</h2>
+            <h2 className='text-[1.5em] mb-2'>Use it for signups</h2>
 
             <p>The PhantomSign email can be used to signup to a service of your choice that may require the email to be verified.</p>
           </div>
@@ -451,7 +451,7 @@ export default function Home() {
           <div className='w-full md:w-1/2 items-center bg-white rounded-lg simple-shadow p-4'>
             <h2 className='text-[1.5em] mb-2'>Receive code or link</h2>
 
-            <p>PhantomSign will try to extract the verificatio code or link from the verification email and output it straight to your screen.</p>
+            <p>PhantomSign will try to extract the verification code or link from the verification email and output it straight to your screen.</p>
           </div>
           <h1 className='text-gray-200 px-8 text-[4em]'>3</h1>
         </div>
