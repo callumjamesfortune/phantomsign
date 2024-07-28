163350388d3463013883469737557c5d40ce2f21
Callum Fortune (Caldog) <63158857+callum-fortune@users.noreply.github.com>
2024-07-28 22:07:35 +0100

    annoying me

--- src/app/(landing page)/client.tsx
+++ src/app/(landing page)/client.tsx
@@ -566,7 +566,7 @@ export default function LandingClient({ user, emailStats }: LandingClientProps) 
             All API requests require a valid API key, which should be provided as a header:
           </p>
 
-          <div className="w-full flex flex-col md:flex-row gap-4 justify-center mb-16">
+          <div className="w-full flex flex-col md:flex-row items-center gap-4 justify-center mb-16">
 
             <code className="self-start bg-white flex gap-4 rounded-md border border-gray-400 px-4 py-2">
               <span className="text-gray-600">x-api-key</span>
