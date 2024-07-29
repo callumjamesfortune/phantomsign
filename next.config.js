94f1998711101acc760a994ebc4b5ef0bf36d8d3
Callum Fortune <callumfortune03@outlook.com>
2024-07-29 08:07:20 +0100

    trying to fix configuration

--- next.config.js
+++ next.config.js
@@ -9,11 +9,21 @@
 // module.exports = nextConfig;
 
 
+// module.exports = {
+//   reactStrictMode: true,
+//   webpack: (config, { isServer }) => {
+//     // Custom webpack configurations (if any)
+//     return config;
+//   },
+// };
+
 module.exports = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    // Custom webpack configurations (if any)
-    return config;
+  swcMinify: true,
+  images: {
+    domains: ['your-domain.com'],
   },
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://your-domain.com' : '',
 };
+
 
