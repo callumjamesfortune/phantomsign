e654dfb93793e3ab84a86931427b69f01e8be550
Callum Fortune <callumfortune03@outlook.com>
2024-08-21 21:27:39 +0100

    maybe it will log

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,17 @@
-import { type NextRequest } from 'next/server'
+
+import { Logger } from 'next-axiom'
+import { NextResponse } from 'next/server'
+import { NextFetchEvent, type NextRequest } from 'next/server'
 import { updateSession } from './utils/supabase/middleware'
 
-export async function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest, event: NextFetchEvent) {
+
+  const logger = new Logger({ source: 'middleware' }); // traffic, request
+  logger.middleware(request)
+
+  event.waitUntil(logger.flush())
+  return NextResponse.next()
+
   return await updateSession(request)
 }
 
--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+const { withAxiom } = require('next-axiom');
+
+module.exports = withAxiom({
   reactStrictMode: true,
   images: {
     domains: ['phantomsign.com'],
   },
-}
+});
