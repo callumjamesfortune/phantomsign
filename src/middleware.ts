972d8f8d7bb0ba8e6744e17afd68722b7fec1b6a
Callum Fortune <callumfortune03@outlook.com>
2024-08-22 08:22:22 +0100

    add rate limit

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,28 @@ import { Logger } from 'next-axiom';
 import { NextResponse } from 'next/server';
 import { NextFetchEvent, type NextRequest } from 'next/server';
 
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+const redis = new Redis({
+  url: "inspired-camel-42383.upstash.io",
+  token: "AaWPAAIjcDE5MmJlNzFjMjBiMGE0NDBmYTc0M2VkZjliYTJkMDE0YnAxMA",
+});
+ 
+const ratelimit = new Ratelimit({
+  redis: redis,
+  limiter: Ratelimit.slidingWindow(5, "10 s"),
+});
+
 export async function middleware(request: NextRequest, event: NextFetchEvent) {
+
+  const ip = request.ip ?? "127.0.0.1";
+  const { success, pending, limit, reset, remaining } =
+    await ratelimit.limit(ip);
+
+  if(!success) {
+    NextResponse.redirect(new URL("/blocked", request.url));
+  } 
 
   const logger = new Logger({ source: 'middleware' }); // traffic, request
 
